4fc7d16bec869adb2909330de7820533da03beaf
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-04-11 00:14:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// const inputs = document.querySelectorAll('.code');
-
-// inputs.forEach((input, index) => {
-//     input.addEventListener('input', (e) => {
-//         if (index < inputs.length - 1 && e.target.value !== '') {
-//             inputs[index + 1].focus();
-//         }
-//     });
-
-//     input.addEventListener('keydown', (e) => {
-//         if (e.key === 'Backspace' && index > 0 && e.target.value === '') {
-//             inputs[index - 1].focus();
-//         }
-//     });
-// });
+let inputs = document.querySelectorAll('.code');
+inputs.forEach((input, index) => {
+    input.addEventListener('keyup', (e) => {
+        if(e.key >= 0 && e.key <= 9) {  // Check if it is a number
+            if(index !== inputs.length-1) {  // Check if it is not the last input field
+                inputs[index + 1].focus();  // Shift the focus to next input field
+            }
+        } else if(e.key === "Backspace") {  // Check if backspace is pressed
+            if(index !== 0) {  // Check if it is not the first input field
+                inputs[index - 1].focus();  // Shift the focus to previous input field
+            }
+        }
+    });
+});
