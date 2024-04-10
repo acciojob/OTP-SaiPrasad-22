e98beb20d95acf56282815963be3717d2ca9b9b4
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-04-10 23:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const inputs = document.querySelectorAll('.code');
+// const inputs = document.querySelectorAll('.code');
 
-inputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-        if (index < inputs.length - 1 && e.target.value !== '') {
-            inputs[index + 1].focus();
-        }
-    });
+// inputs.forEach((input, index) => {
+//     input.addEventListener('input', (e) => {
+//         if (index < inputs.length - 1 && e.target.value !== '') {
+//             inputs[index + 1].focus();
+//         }
+//     });
 
-    input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && index > 0 && e.target.value === '') {
-            inputs[index - 1].focus();
-        }
-    });
-});
+//     input.addEventListener('keydown', (e) => {
+//         if (e.key === 'Backspace' && index > 0 && e.target.value === '') {
+//             inputs[index - 1].focus();
+//         }
+//     });
+// });
